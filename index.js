814a06d7e336e93a0a72acfbddf03297817f001b
Renzo-TEN3T <84866105+Renzo-TEN3T@users.noreply.github.com>
2024-02-20 00:17:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,85 +1,38 @@
-const chatbotToggler = document.querySelector(".chatbot-toggler");
-const closeBtn = document.querySelector(".close-btn");
-const chatbox = document.querySelector(".chatbox");
-const chatInput = document.querySelector(".chat-input textarea");
-const sendChatBtn = document.querySelector(".chat-input span");
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Carta de San Valentín</title>
+    <link rel="stylesheet" href="./style.css" />
+  </head>
+  <body>
+    <div class="valentines-day">
+      <div class="envelope"></div>
+      <div class="heart"></div>
+      <div class="text">HAPPY <br />BIRTHDAY MY <br />LOVE!</div>
+      <div class="front"></div>
+    </div>
 
-let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
-const inputInitHeight = chatInput.scrollHeight;
+    <div id="card">
+      <div class="side one"></div>
+      <div class="side two">
+        <h2>Estrella Rojas</h2>
+        <p>Feliz cumpleaños, Mi amor 💕</p>
+        <p>
+          Me encanta verte feliz, me encanta cuando hablamos de nuestro futuro,
+          sé que por ahora solo son palabras, pero sé que juntos, tenemos el
+          poder de alcanzar cualquier meta que nos propongamos.
+        </p>
+        <p>
+          Eres mi compañera de vida y mi mayor apoyo. No hay nada que no pueda
+          enfrentar a tu lado. Juntos, somos invencibles 💕. Te amo, más de lo
+          que las palabras pueden expresar, y estoy emocionado por todo lo que
+          el futuro nos depara. Con todo mi amor, Renzito :3
+        </p>
+      </div>
+    </div>
 
-const createChatLi = (message, className) => {
-    // Create a chat <li> element with passed message and className
-    const chatLi = document.createElement("li");
-    chatLi.classList.add("chat", `${className}`);
-    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
-    chatLi.innerHTML = chatContent;
-    chatLi.querySelector("p").textContent = message;
-    return chatLi; // return chat <li> element
-}
-
-const generateResponse = (chatElement) => {
-    const API_URL = "https://api.openai.com/v1/chat/completions";
-    const messageElement = chatElement.querySelector("p");
-
-    // Define the properties and message for the API request
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${API_KEY}`
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: userMessage}],
-        })
-    }
-
-    // Send POST request to API, get response and set the reponse as paragraph text
-    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
-        messageElement.textContent = data.choices[0].message.content.trim();
-    }).catch(() => {
-        messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Hubo algún error. Por favor intentar de nuevo.";
-    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-}
-
-const handleChat = () => {
-    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
-    if(!userMessage) return;
-
-    // Clear the input textarea and set its height to default
-    chatInput.value = "";
-    chatInput.style.height = `${inputInitHeight}px`;
-
-    // Append the user's message to the chatbox
-    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
-    chatbox.scrollTo(0, chatbox.scrollHeight);
-    
-    setTimeout(() => {
-        // Display "Thinking..." message while waiting for the response
-        const incomingChatLi = createChatLi("Thinking...", "incoming");
-        chatbox.appendChild(incomingChatLi);
-        chatbox.scrollTo(0, chatbox.scrollHeight);
-        generateResponse(incomingChatLi);
-    }, 600);
-}
-
-chatInput.addEventListener("input", () => {
-    // Adjust the height of the input textarea based on its content
-    chatInput.style.height = `${inputInitHeight}px`;
-    chatInput.style.height = `${chatInput.scrollHeight}px`;
-});
-
-chatInput.addEventListener("keydown", (e) => {
-    // If Enter key is pressed without Shift key and the window 
-    // width is greater than 800px, handle the chat
-    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
-        e.preventDefault();
-        handleChat();
-    }
-});
-
-sendChatBtn.addEventListener("click", handleChat);
-closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
-chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="./script.js"></script>
+  </body>
+</html>
